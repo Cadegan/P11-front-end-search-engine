093636a7c11f3f1b541248c525f051b7d884a356
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-03-30 10:50:16 +0900

    Sauvegarde

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-import recipes from '../data/recipes';
+import { recipes } from '../data/recipes';
 import RecipeData from '../factories/models/recipeData';
 import RecipeTemplate from '../factories/templates/recipeTemplate';
 
 const galleryContainer = document.querySelector('.gallery-container');
 const gallery = [];
 
-function showGallery(recipes) {
+function showGallery() {
   recipes.forEach((recipe) => {
     const recipeData = new RecipeData(recipe);
     const recipeTemplate = new RecipeTemplate(recipeData);
-
+    // galleryContainer.appendChild(recipeTemplate);
     galleryContainer.innerHTML += recipeTemplate;
   });
 }
