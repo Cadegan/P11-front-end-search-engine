8821066c9d9d4517eb917385a9ab0e0d025e255e
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-03-31 15:55:07 +0900

    Saurecipes.js => json

--- factories/templates/recipeTemplate.js
+++ factories/templates/recipeTemplate.js
@@ -12,33 +12,44 @@ export default class RecipeTemplate {
     this.container.innerHTML = this.templateOfRecipe();
   }
 
-  getIngredientsData (i) {
-    let details = "";
-    let ingredient;
-    let quantity;
-    let unit;
-    for (let j = 0; j < this.recipes.length; j++ ) {
-      ingredient =
-        this.recipes[i].ingredients[j].ingredient
+  templateOfRecipe() {
+    let recipeCard = "";
+    for (let i = 0; i < this.recipes.length; i++) {
+      recipeCard += `
+        <article class="recipe">
+          <img src="" alt="Recipe picture" >
+            <div class="recipeHeading">
+              <p class="recipeTitel">${this.recipes[i].name}</p>
+              <img src="" alt="Clock logo">
+              <p class="recipeTime">${this.recipes[i].time}</p>
+            </div>
+            <div class="recipeBody">
+              <div class="allNeedCook">${""}</div>
+              <div class="explanationRecipe">${this.recipes[i].description}</div>
+            </div>
+        </article>
+        `;
     }
-
+    return recipeCard;
   }
 
-  // templateOfRecipe() {
-  //   return `
-  //       <article class="recipe">
-  //           <img src="" alt="Recipe picture" >
-  //           <div class="recipeHeading">
-  //               <p class="recipeTitel">${this._recipe.name}</p>
-  //               <img src="" alt="Clock logo">
-  //               <p class="recipeTime">${this._recipe.time}</p>
-  //           </div>
-  //           <div class="recipeBody">
-  //               <div class="allNeedCook">${this._recipe.ingredients}</div>
-  //               <div class="explanationRecipe">${this._recipe.description}</div>
-  //           </div>
-  //       </article>
-  //     `;
+  // getIngredientsData(i) {
+  //   let details = "";
+  //   let ingredient;
+  //   // let quantity;
+  //   // let unit;
+  //   for (let j = 0; j < this.recipes.length; j++) {
+  //     ingredient = this.recipes[i].ingredients[j].ingredient !== undefined
+  //      ? this.recipes[i].ingredients[j].ingredient : "";
+  //     details += `
+  //   <li>
+  //     <span class="ingredient">
+  //       ${ingredient}
+  //     </span>
+  //   </li>
+  //   `;
+  //   }
+  //   return details;
   // }
 }
 
--- js/index.js
+++ js/index.js
@@ -1,33 +1,35 @@
-import { recipes } from "../data/recipes.js";
-import RecipeData from "../factories/models/recipeData.js";
-import RecipeTemplate from "../factories/templates/recipeTemplate.js";
-
-const recipesArray = Object.entries(recipes);
-// console.log(recipesArray);
-// console.log(recipes)
-
-const galleryContainer = document.querySelector(".gallery-container");
-const gallery = [];
-
-function showGallery() {
-  recipesArray.forEach((recipe) => {
-    const recipeData = new RecipeData(recipe);
-    const recipeTemplate = new RecipeTemplate(recipeData);
-    // galleryContainer.appendChild(recipeTemplate);
-    galleryContainer.innerHTML += recipeTemplate;
-  });
-}
-
-const recipesDisplay = () => {
-  galleryContainer.innerHTML = "";
-  recipesArray.forEach((recipe) => {
-    gallery.push(recipe);
-  });
-  showGallery(gallery);
-};
+import { recipes } from '../data/recipes.js';
+import RecipeData from '../factories/models/recipeData.js';
+import RecipeTemplate from '../factories/templates/recipeTemplate.js';
 
 async function init() {
-  recipesDisplay();
+  let data = await fetch(recipes);
+  data = await data.json;
+  window.recipeCards = new RecipeTemplate(data);
 }
+
+// const galleryContainer = document.querySelector('.gallery-container');
+// const gallery = [];
+
+// function showGallery() {
+//   recipes.forEach((recipe) => {
+//     const recipeData = new RecipeData(recipe);
+//     const recipeTemplate = new RecipeTemplate(recipeData);
+//     // galleryContainer.appendChild(recipeTemplate);
+//     galleryContainer.innerHTML += recipeTemplate;
+//   });
+// }
+
+// const recipesDisplay = () => {
+//   galleryContainer.innerHTML = '';
+//   recipes.forEach((recipe) => {
+//     gallery.push(recipe);
+//   });
+//   showGallery(gallery);
+// };
+
+// async function init() {
+//   recipesDisplay();
+// }
 
 init();
