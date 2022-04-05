74526e7bac0961305f309f0bf395d9d1c4584376
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-04-05 14:56:27 +0900

    Schéma 📈

--- js/index.js
+++ js/index.js
@@ -9,28 +9,3 @@ async function init() {
 }
 
 init();
-
-// const galleryContainer = document.querySelector('.gallery-container');
-// const gallery = [];
-
-// function showGallery() {
-//   recipes.forEach((recipe) => {
-//     const recipeData = new RecipeData(recipe);
-//     const recipeTemplate = new RecipeTemplate(recipeData);
-//     // galleryContainer.appendChild(recipeTemplate);
-//     galleryContainer.innerHTML += recipeTemplate;
-//   });
-// }
-
-// const recipesDisplay = () => {
-//   galleryContainer.innerHTML = '';
-//   recipes.forEach((recipe) => {
-//     gallery.push(recipe);
-//   });
-//   showGallery(gallery);
-// };
-
-// async function init() {
-//   recipesDisplay();
-// }
-
