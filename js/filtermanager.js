08af60d4e778b6007232c7e317afe807d16932db
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-04-11 18:46:00 +0900

    mainFilter v0

--- js/filtermanager.js
+++ js/filtermanager.js
@@ -1,6 +1,21 @@
 export default class filterManager {
   constructor(data) {
     this.recipes = data;
-    
   }
+
+  // dataFiltred() {
+  //   const resultFiltred = [];
+  //   let card;
+  //   for (const value of Object.values(this.recipes)) {
+
+  //   }
+  // }
+
+  // mainInsputIngredients(value) {
+  //   for (let i = value.lengh - 1; i >= 0; i--) {
+  //     if (value[i].ingredient.includes(value)) {
+  //       return true;
+  //     }
+  //   } return false;
+  // }
 }
