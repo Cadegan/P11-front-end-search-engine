e06e35238bdf127b6f29f4bffd34bbba1a94247e
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-04-12 16:34:49 +0900

    Debut construction filtre if/for

--- js/filtermanager.js
+++ js/filtermanager.js
@@ -18,4 +18,29 @@ export default class filterManager {
   //     }
   //   } return false;
   // }
+
+  for(let j = 0; j <this.recipes[i].ingredients.length; j++) {
+  ingredient = this.recipes[i].ingredients[j].ingredient !== undefined ? this.recipes[i].ingredients[j].ingredient : "";
+  quantity = this.recipes[i].ingredients[j].quantity !== undefined ? this.recipes[i].ingredients[j].quantity : "";
+  unit = this.recipes[i].ingredients[j].unit !== undefined ? this.recipes[i].ingredients[j].unit : "";
+    
+}
+
+  mainInputSearch () {
+    const result = document.querySelector("#mainSearchBar").value.toLowerCase();
+
+    if (result.lenght >= 3) {
+      const newDataFiltred = [];
+      
+      for (let i = 0; i < recipes.lenght; i++) {
+        const activRecipes = recipes[i];
+        let activIngredients = false;
+        for (let j = 0; j < activRecipes.ingredients.length; j++) {
+          if (activRecipes.ingredients[j].ingredient.toLowerCase().includes(result)) {
+            activIngredients = true;
+          }
+        }
+      }
+    }
+  }
 }
