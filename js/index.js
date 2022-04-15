20ed445a39c178522b173c7e8f89e3ccae6d24f0
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-04-15 12:21:45 +0900

    Conversion json => js

--- js/index.js
+++ js/index.js
@@ -2,17 +2,26 @@
 import RecipeTemplate from "../factories/templates/recipeTemplate.js";
 import mainInputSearch from "./searchBar.js";
 
-let data = await fetch("../data/recipes.json");
+// let data = await fetch("../data/recipes.json");
 
 export default async function init() {
-  data = await data.json();
-  window.recipeCards = new RecipeTemplate(data);
+  // data = await data.json();
+  // eslint-disable-next-line no-undef
+  window.recipeCards = new RecipeTemplate(recipes);
   // window.searchBar = new SearchBar(data);
 }
 
 const update = () => {
-  init(data);
+  init();
   mainInputSearch();
 };
 
 update();
+
+// async function initD () {
+//   await fetch("../data/recipes.json");
+//   .then(res => res.json())
+//   .then((data) => {
+
+//   })
+// }
