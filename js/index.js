964e910a590cd9ce0047413962386a16d364635c
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-04-18 14:45:20 +0900

    Uncaught : updateRecipes

--- js/index.js
+++ js/index.js
@@ -1,27 +1,18 @@
 /* eslint-disable import/extensions */
 import RecipeTemplate from "../factories/templates/recipeTemplate.js";
-import mainInputSearch from "./searchBar.js";
 
 // let data = await fetch("../data/recipes.json");
 
-export default async function init() {
+const updateRecipes = (data) => {
   // data = await data.json();
   // eslint-disable-next-line no-undef
-  window.recipeCards = new RecipeTemplate(recipes);
+  window.recipeCards = new RecipeTemplate(data);
   // window.searchBar = new SearchBar(data);
-}
+};
 
-const update = () => {
-  init();
+const init = () => {
+  updateRecipes(recipes);
   mainInputSearch();
 };
 
-update();
-
-// async function initD () {
-//   await fetch("../data/recipes.json");
-//   .then(res => res.json())
-//   .then((data) => {
-
-//   })
-// }
+init();
--- js/searchBar.js
+++ js/searchBar.js
@@ -1,13 +1,13 @@
 // import init from "./index";
 
-export default function mainInputSearch() {
+const mainInputSearch = () => {
+  const filtredRecipes = recipes;
   const inputEnter = document.querySelector("#mainSearchBar").value.toLowerCase();
 
   if (inputEnter.lenght >= 3) {
     const dataFiltred = [];
-
-    for (let i = 0; i < this.recipes.lenght; i += 1) {
-      const activRecipes = this.recipes[i];
+    for (let i = 0; i < filtredRecipes.lenght; i += 1) {
+      const activRecipes = filtredRecipes[i];
       let activIngredients = false;
       for (let j = 0; j < activRecipes.ingredients.length; j += 1) {
         if (activRecipes.ingredients[j].ingredient.toLowerCase().includes(inputEnter)) {
@@ -15,15 +15,19 @@ export default function mainInputSearch() {
         }
       }
       if (activRecipes.name.toLowerCase().includes(inputEnter)
-                    || activRecipes.description.toLowerCase().includes(inputEnter)
-                    || activIngredients) {
+          || activRecipes.description.toLowerCase().includes(inputEnter)
+          || activIngredients) {
         dataFiltred.push(activRecipes);
       }
     }
-    if (this.recipes.lenght === 0) {
+    if (filtredRecipes.lenght === 0) {
       document.querySelector(".listRecipesSection").textContent = "Aucune recette n'a été tourvée...";
     } else {
-      init(dataFiltred);
+      updateRecipes(dataFiltred);
     }
+  } else {
+    updateRecipes(filtredRecipes);
   }
-}
+};
+
+document.getElementById("mainSearchBar").addEventListener("input", mainInputSearch);
