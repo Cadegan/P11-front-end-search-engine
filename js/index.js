6496a22f989b166c5452d1f414a5440f42244af7
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-04-18 22:12:54 +0900

    filterList bug : affichage null 🪳

--- js/index.js
+++ js/index.js
@@ -1,11 +1,42 @@
 /* eslint-disable import/extensions */
 import RecipeTemplate from "../factories/templates/recipeTemplate.js";
 
-// let data = await fetch("../data/recipes.json");
+const filterList = (data) => {
+  const ingredientsList = [];
+  const applianceList = [];
+  const ustensilsList = [];
+  const allLists = [ingredientsList, applianceList, ustensilsList];
+  const article = document.querySelectorAll(".panel-body").forEach((filtersList) => filtersList.innerHTML = "");
+
+  data.forEach((recipe) => {
+    recipe.ingredients.forEach((item) => (ingredientsList.indexOf(item.ingredient) === -1 ? ingredientsList.push(item.ingredient) : ""));
+  });
+
+  allLists.forEach((list) => {
+    list.forEach((item) => {
+      const itemList = article.content.cloneNode(true).children[1];
+      itemList.firstElementChild.textContent = `${item.slice(0, 1).toUpperCase()}${item.slice(1).toLowerCase()}`;
+
+      const addToFilterList = (filterType) => {
+        const inputFilterList = document.getElementById(`${filterType}-SearchInput`).value.toLowerCase();
+        if (itemList.firstElementChild.textContent.toLowerCase().includes(inputFilterList)) {
+          document.getElementById(`${filterType}-list`).appendChild(itemList);
+        }
+
+        switch (list) {
+          case ingredientsList:
+            addToFilterList("ingredients");
+            break;
+        }
+      };
+    });
+  });
+};
 
 const updateRecipes = (data) => {
   // data = await data.json();
   window.recipeCards = new RecipeTemplate(data);
+  filterList(data);
   // window.searchBar = new SearchBar(data);
 };
 
@@ -15,14 +46,3 @@ const init = () => {
 };
 
 init();
-
-const filterList = (data) => {
-  const ingredientsList = [];
-  const applianceList = [];
-  const ustensilsList = [];
-
-  
-
-}
-
-// document.getElementById("main-search").addEventListener("input", mainInputSearch);
