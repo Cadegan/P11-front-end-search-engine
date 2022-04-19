f92097e76466312f5447223efc877a8e7ab1f5b9
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-04-19 11:14:50 +0900

    Index clean

--- js/index.js
+++ js/index.js
@@ -1,42 +1,10 @@
 /* eslint-disable import/extensions */
 import RecipeTemplate from "../factories/templates/recipeTemplate.js";
 
-const filterList = (data) => {
-  const ingredientsList = [];
-  const applianceList = [];
-  const ustensilsList = [];
-  const allLists = [ingredientsList, applianceList, ustensilsList];
-  const article = document.querySelectorAll(".panel-body").forEach((filtersList) => filtersList.innerHTML = "");
-
-  data.forEach((recipe) => {
-    recipe.ingredients.forEach((item) => (ingredientsList.indexOf(item.ingredient) === -1 ? ingredientsList.push(item.ingredient) : ""));
-  });
-
-  allLists.forEach((list) => {
-    list.forEach((item) => {
-      const itemList = article.content.cloneNode(true).children[1];
-      itemList.firstElementChild.textContent = `${item.slice(0, 1).toUpperCase()}${item.slice(1).toLowerCase()}`;
-
-      const addToFilterList = (filterType) => {
-        const inputFilterList = document.getElementById(`${filterType}-SearchInput`).value.toLowerCase();
-        if (itemList.firstElementChild.textContent.toLowerCase().includes(inputFilterList)) {
-          document.getElementById(`${filterType}-list`).appendChild(itemList);
-        }
-
-        switch (list) {
-          case ingredientsList:
-            addToFilterList("ingredients");
-            break;
-        }
-      };
-    });
-  });
-};
-
 const updateRecipes = (data) => {
   // data = await data.json();
   window.recipeCards = new RecipeTemplate(data);
-  filterList(data);
+  // filterList(data);
   // window.searchBar = new SearchBar(data);
 };
 
