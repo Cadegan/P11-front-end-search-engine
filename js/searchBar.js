532af0fe4571192886b4b64873f700476ec07420
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-04-27 15:08:44 +0900

    Renommage

--- js/searchBar.js
+++ js/searchBar.js
@@ -1,26 +1,29 @@
 import { updateRecipes } from "./index.js";
 
 function functionSearch() {
-  let filtredRecipes = recipes;
-  const mainInputSearch = document.querySelector("#mainInputSearch").value.toLowerCase();
+  let RecipesFiltredByTag = recipes;
+  const mainInputSearch = document.getElementById("mainInputSearch").value.toLowerCase();
 
-  // TagSearch
+  // TagSelected
   document.querySelectorAll("#tagSection li").forEach((tag) => {
-    const tagSearchInput = tag.textContent.toLowerCase();
-    filtredRecipes = filtredRecipes.filter((recipe) =>
+    const tagSelected = tag.textContent.toLowerCase();
+    RecipesFiltredByTag = RecipesFiltredByTag.filter((recipe) =>
       // filter() retourne un nouveau tableau si le callback some(e) retourne "true"
-      // cad si un element dans "recipes" correspond au "tagSearchInput"
-      recipe.ingredients.some((e) => e.ingredient.toLowerCase().includes(tagSearchInput))
-      || recipe.appliance.toLowerCase().includes(tagSearchInput)
-      || recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(tagSearchInput)));
-    updateRecipes(filtredRecipes);
-    console.log("filtredRecipes =", filtredRecipes);
+      // cad si un element dans "recipes" correspond au "tagSelected"
+      recipe.ingredients.some((e) => e.ingredient.toLowerCase().includes(tagSelected))
+      || recipe.appliance.toLowerCase().includes(tagSelected)
+      || recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(tagSelected)));
+    updateRecipes(RecipesFiltredByTag);
+    console.log("RecipesFiltredByTag =", RecipesFiltredByTag);
   });
 
+  // Input search
   if (mainInputSearch.lenght >= 3) {
     const dataFiltred = [];
-    for (let i = 0; i < filtredRecipes.lenght; i += 1) {
-      const activRecipes = filtredRecipes[i];
+    // Pour chercher les ingredients il faut faire une double boucle :
+    // parmi les recettes puis les ingredients disponibles
+    for (let i = 0; i < RecipesFiltredByTag.lenght; i += 1) {
+      const activRecipes = RecipesFiltredByTag[i];
       let activIngredients = false;
       for (let j = 0; j < activRecipes.ingredients.length; j += 1) {
         if (activRecipes.ingredients[j].ingredient.toLowerCase().includes(mainInputSearch)) {
@@ -33,24 +36,23 @@ function functionSearch() {
         dataFiltred.push(activRecipes);
       }
     }
-    if (filtredRecipes.lenght === 0) {
+    if (RecipesFiltredByTag.lenght === 0) {
       document.querySelector(".listRecipesSection").textContent = "Aucune recette n'a été tourvée...";
     } else {
       updateRecipes(dataFiltred);
     }
     console.log("dataFiltred =", dataFiltred);
   } else {
-    updateRecipes(filtredRecipes);
+    updateRecipes(RecipesFiltredByTag);
   }
 }
 
-const inputSearchEvents = () => {
-  const tagSearchInput = document.querySelectorAll(".search-input");
-  tagSearchInput.forEach((input) => {
+function inputSearchEvents() {
+  const searchInput = document.querySelectorAll(".search-input");
+  searchInput.forEach((input) => {
     input.addEventListener("input", functionSearch);
   });
-};
-
-// document.getElementById("mainInputSearch").addEventListener("input", mainInputSearch);
+  document.getElementById("mainInputSearch").addEventListener("input", functionSearch);
+}
 
 export { functionSearch, inputSearchEvents };
