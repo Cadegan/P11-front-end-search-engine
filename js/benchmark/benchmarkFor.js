57e4340d8b19e4fb7cbfe91953c5654e1b55b53d
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-05-04 20:55:25 +0900

    Creation benchmark For

---
+++ js/benchmark/benchmarkFor.js
@@ -0,0 +1,19 @@
+const functionSearch = (filter) => {
+  const dataFiltredByInput = [];
+  for (let i = 0; i < recipes.length; i += 1) {
+    const activeRecipesByTag = recipes[i];
+    let IngredientIsVisible = false;
+    for (let j = 0; j < activeRecipesByTag.ingredients.length; j += 1) {
+      if (activeRecipesByTag.ingredients[j].ingredient.toLowerCase().includes(filter)) {
+        IngredientIsVisible = true;
+      }
+    }
+    if (activeRecipesByTag.name.toLowerCase().includes(filter)
+            || activeRecipesByTag.description.toLowerCase().includes(filter)
+            || IngredientIsVisible) {
+      dataFiltredByInput.push(activeRecipesByTag);
+    }
+  }
+};
+
+functionSearch("chocolat");
